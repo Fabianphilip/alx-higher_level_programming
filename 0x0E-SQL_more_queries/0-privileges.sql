e3910ca7b5daf7a58fcc11f6cea05530b68f4930
Fabian <125877740+Fabianphilip@users.noreply.github.com>
2023-08-16 16:58:19 -0700

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- show grant privileges for an unknown user
+-- Lists all privileges of the users user_0d_1 and user_0d_2.
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
