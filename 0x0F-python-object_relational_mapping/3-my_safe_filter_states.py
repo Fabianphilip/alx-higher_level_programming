00fd06bd8c3c23de4182347e6b5a58f13f2ee9d1
Fabianphilip <fabianphilip92@yahoo.com>
2023-09-18 04:42:02 -0700

    SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,6 +6,5 @@ import MySQLdb
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name = '{}'"
-                .format(sys.argv[4]))
-    [print(state) for state in cur.fetchall()]
+    cur.execute("SELECT * FROM states WHERE name=%s;", (sys.argv[4],))
+    [print(states) for states in cur.fetchall()]
