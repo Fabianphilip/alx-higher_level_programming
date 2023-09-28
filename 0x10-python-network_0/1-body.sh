e5936fd7bf0fed39b7c1727e6f3c50ac7a9ffad3
Fabian <125877740+Fabianphilip@users.noreply.github.com>
2023-09-28 06:03:36 -1200

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# send a GET request to an URL with curl, and display the body of the response
-curl -sL "$1"
+# take in a URL, send a GET req, display the body of the response
+curl -Lfs $1
